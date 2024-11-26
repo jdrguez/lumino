5e51178cb9ff552d953f486ee6b9d978b39d84ec
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-11-26 20:14:06 +0000

    fix models

--- lumino/subjects/models.py
+++ lumino/subjects/models.py
@@ -1,3 +1,46 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Subject(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='subjects', on_delete=models.CASCADE
+    )
+    students = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        through='Enrollment',
+        through_fields=('student', 'subject'),
+    )
+
+    class Meta:
+        ordering = ['code']
+
+    def __str__(self):
+        return f'{self.code}: {self.name}'
+
+    def get_absolute_url(self):
+        return reverse('subjects:subject-detail', kwargs={'subject_pk': self.pk})
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    subject = models.ForeignKey(
+        'subjects.Subject',
+        related_name='lessons',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('subjects:lesson-detail', kwargs={'lesson_pk': self.pk})
--- lumino/users/models.py
+++ lumino/users/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='s', on_delete=models.CASCADE
+    )
