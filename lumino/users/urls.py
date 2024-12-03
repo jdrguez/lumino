0fbf78feeccef0c0b98ee09db46e2275ba3d6258
nicholelouis <nicholelouiss@gmail.com>
2024-12-03 18:41:56 +0000

    a

---
+++ lumino/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('edit/', views.edit_profile, name='edit-profile'),
+    path('certificate/', views.request_certificate, name='request-certificate'),
+    path('enroll/', views.enroll_subjects, name='enroll-subjects'),
+    path('unenroll/', views.unenroll_subjects, name='unenroll-subjects'),
+    path('leave/', views.leave, name='leave'),
+    ]
--- lumino/users/views.py
+++ lumino/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def edit_profile():
+    pass
+
+def request_certificate():
+    pass
+
+def enroll_subjects():
+    pass
+
+def unenroll_subjects():
+    pass
+
+def leave():
+    pass
