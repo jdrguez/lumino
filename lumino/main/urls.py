5cbe0f3dd24921edea3e8020469982f3aaa4c518
jdrguez <josedomingo.rguez.rguez@gmail.com>
2024-12-03 20:35:55 +0000

    add landing

--- lumino/main/urls.py
+++ lumino/main/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 
 import accounts.views
+import shared.views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', shared.views.landing, name='landing'),
     path('admin/', admin.site.urls),
     path('login/', accounts.views.user_login, name='login'),
     path('logout/', accounts.views.user_logout, name='logout'),
