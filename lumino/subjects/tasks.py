5224437d86a0c94104908849e788f4aa545cfe0a
jdrguez <josedomingo.rguez.rguez@gmail.com>
2025-01-16 20:21:09 +0000

    fix email

--- lumino/subjects/tasks.py
+++ lumino/subjects/tasks.py
@@ -5,14 +5,24 @@ from django_rq import job
 from markdown import markdown
 from weasyprint import HTML
 
+from .models import Enrollment
+
 
 @job
 def deliver_certificate(base_url, student):
-    rendered = render_to_string('subjects/certificates/certificates.html')
+    enrollments = Enrollment.objects.filter(student=student)
+    rendered = render_to_string(
+        'subjects/certificates/certificates.html', {'student': student, 'enrollments': enrollments}
+    )
     output_path = settings.CERTIFICATE_DIR / f'{student.username}_grade_certificate.pdf'
     HTML(string=rendered, base_url=base_url).write_pdf(output_path)
-    rendered = render_to_string('subjects/certificates/email.md')
-    email = EmailMessage(subject='A', body=markdown(rendered), from_email=None, to=[student.email])
+    rendered = render_to_string('subjects/certificates/email.md', {'student': student})
+    email = EmailMessage(
+        subject='Tu certificado de notas de lumino',
+        body=markdown(rendered),
+        from_email=None,
+        to=[student.email],
+    )
     email.content_subtype = 'html'
     email.attach_file(str(output_path))
     email.send()
