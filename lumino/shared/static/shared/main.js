481df092a16ca0268105e2d667924ccc6e6370f0
jdrguez <josedomingo.rguez.rguez@gmail.com>
2025-01-17 12:19:01 +0000

    add js

--- lumino/shared/static/shared/main.js
+++ lumino/shared/static/shared/main.js
@@ -1,26 +1,22 @@
 const mainContainer = document.getElementById("mainContainer");
-    const sidebar = document.getElementById("sidebar");
-    const toggleButton = document.getElementById("toggleButton");
+const sidebar = document.getElementById("sidebar");
+const toggleButton = document.getElementById("toggleButton");
 
-    // Cargar estado inicial del sidebar desde localStorage
-    const isSidebarVisible = localStorage.getItem("isSidebarVisible") !== "false";
+const isSidebarVisible = localStorage.getItem("isSidebarVisible") !== "false";
 
-    function updateLayout(showSidebar) {
-      if (showSidebar) {
-        mainContainer.classList.remove("full");
-        sidebar.style.display = "block";
-      } else {
-        mainContainer.classList.add("full");
-        sidebar.style.display = "none";
-      }
-      localStorage.setItem("isSidebarVisible", showSidebar);
+function updateLayout(showSidebar) {
+    if (showSidebar) {
+    mainContainer.classList.remove("full");
+    sidebar.style.display = "block";
+    } else {
+    mainContainer.classList.add("full");
+    sidebar.style.display = "none";
     }
+    localStorage.setItem("isSidebarVisible", showSidebar);
+}
 
-    // Toggle sidebar al presionar el botón
-    toggleButton.addEventListener("click", () => {
-      const isVisible = sidebar.style.display !== "none";
-      updateLayout(!isVisible);
-    });
-
-    // Configuración inicial
-    updateLayout(isSidebarVisible);
+toggleButton.addEventListener("click", () => {
+    const isVisible = sidebar.style.display !== "none";
+    updateLayout(!isVisible);
+});
+updateLayout(isSidebarVisible);
